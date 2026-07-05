b7a8b45cef5b8a1bc6cc231b9504aabb7975c10d
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-05 20:44:52 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,49 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
+  "./app.js?v=4",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v4";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
   "./icon.svg"
