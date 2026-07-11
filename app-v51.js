e24e30e0409b1a1315b85a75d8eeb964ab21d757
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-11 19:32:59 +0800

    Add v51 dashboard loader

---
+++ app-v51.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(() => {
+  const serviceWorker = navigator.serviceWorker;
+  const originalRegister = serviceWorker?.register?.bind(serviceWorker);
+
+  if (originalRegister) {
+    serviceWorker.register = (url, options) => {
+      const nextUrl = String(url || "").replace("service-worker-v47.js", "service-worker-v51.js");
+      return originalRegister(nextUrl, options);
+    };
+  }
+
+  const script = document.createElement("script");
+  script.src = "app-v47.js?v=51";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
