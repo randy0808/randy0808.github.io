40674ff2cdeb17c55f3187bc5c2b8621232b2bda
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-12 20:27:52 +0800

    Update service-worker-v56.js for asset growth history

---
+++ service-worker-v56.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v56";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./asset-detail.html",
+  "./styles-v56.css",
+  "./app-v56.js",
+  "./app-v47.js?v=56",
+  "./asset-detail-v53.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
