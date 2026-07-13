f725cdb9e0e0552f1f10b30573a1946b78fdc7ea
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-14 02:31:42 +0800

    Publish wealthtrack v71 lock dividend source scroll

---
+++ service-worker-v71.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v71";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./asset-detail.html",
+  "./styles-v56.css",
+  "./layout-fix-v57.css",
+  "./dashboard-tabs-v58.css",
+  "./overview-dividends-v69.css",
+  "./holdings-sticky-v65.css",
+  "./overview-dividends-fix-v70.js?v=71",
+  "./dividend-scroll-v71.js?v=71",
+  "./holdings-sticky-v65.js?v=71",
+  "./app-v58.js?v=71",
+  "./app-v47.js?v=71",
+  "./asset-detail-v53.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
