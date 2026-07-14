92651dc68266ac7487f4f5083de9757b6f17d041
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-15 03:04:00 +0800

    Publish wealthtrack v83 fundamentals completeness

---
+++ service-worker-v83.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v83";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./asset-detail.html",
+  "./styles-v56.css?v=83",
+  "./layout-fix-v57.css",
+  "./dashboard-tabs-v58.css",
+  "./overview-dividends-v69.css",
+  "./overview-dividends-sort-v75.css?v=83",
+  "./holdings-sticky-v65.css",
+  "./growth-history-v73.js?v=83",
+  "./growth-chart-hover-v74.js?v=83",
+  "./overview-dividends-sort-v75.js?v=83",
+  "./holdings-sticky-v65.js?v=83",
+  "./app-v58.js?v=83",
+  "./app-v47.js?v=83",
+  "./asset-detail-v53.js?v=83",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
