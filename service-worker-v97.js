c168b3e4c71b140c0d7281b87224b5bf1008cadd
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-16 05:00:34 +0800

    Publish wealthtrack v97 exclude banks from growth alerts

---
+++ service-worker-v97.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v97";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./asset-detail.html",
+  "./styles-v56.css?v=97",
+  "./layout-fix-v57.css",
+  "./dashboard-tabs-v58.css",
+  "./overview-dividends-v69.css",
+  "./overview-dividends-sort-v75.css?v=97",
+  "./holdings-sticky-v65.css",
+  "./growth-history-v73.js?v=97",
+  "./growth-chart-hover-v74.js?v=97",
+  "./overview-dividends-sort-v75.js?v=97",
+  "./holdings-sticky-v65.js?v=97",
+  "./app-v58.js?v=97",
+  "./app-v47.js?v=97",
+  "./asset-detail-v53.js?v=90",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
