f8c91a708a164b219993bb1a86d955338f052b27
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-21 04:22:14 +0800

    Publish wealthtrack v106 prevent edit recursion

--- app-v58.js
+++ app-v58.js
@@ -6,7 +6,7 @@
 
   if (originalRegister) {
     serviceWorker.register = (url, options) => {
-      const nextUrl = String(url || "").replace("service-worker-v47.js", "service-worker-v105.js");
+      const nextUrl = String(url || "").replace("service-worker-v47.js", "service-worker-v106.js");
       return originalRegister(nextUrl, options);
     };
   }
@@ -153,10 +153,10 @@
   }
 
   ensureV58Layout();
-  loadScript("app-v47.js?v=105")
-    .then(() => loadScript("growth-history-v73.js?v=105"))
-    .then(() => loadScript("growth-chart-hover-v74.js?v=105"))
-    .then(() => loadScript("overview-dividends-sort-v75.js?v=105"))
-    .then(() => loadScript("holdings-sticky-v65.js?v=105"))
+  loadScript("app-v47.js?v=106")
+    .then(() => loadScript("growth-history-v73.js?v=106"))
+    .then(() => loadScript("growth-chart-hover-v74.js?v=106"))
+    .then(() => loadScript("overview-dividends-sort-v75.js?v=106"))
+    .then(() => loadScript("holdings-sticky-v65.js?v=106"))
     .catch((error) => console.warn("WealthTrack v58 patch failed", error));
 })();
