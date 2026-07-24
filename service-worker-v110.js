d3cb4adb3b48ed610786c76d47d1a86969733258
randy0808 <143749015+randy0808@users.noreply.github.com>
2026-07-24 18:06:25 +0800

    Publish wealthtrack v110 align dividend source height

---
+++ service-worker-v110.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const CACHE_NAME = "wealthtrack-v110";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./asset-detail.html",
+  "./styles-v56.css?v=104",
+  "./layout-fix-v57.css",
+  "./dashboard-tabs-v58.css?v=108",
+  "./overview-dividends-v69.css?v=110",
+  "./overview-dividends-sort-v75.css?v=107",
+  "./holdings-sticky-v65.css",
+  "./growth-history-v73.js?v=110",
+  "./growth-chart-hover-v74.js?v=110",
+  "./overview-dividends-sort-v75.js?v=110",
+  "./holdings-sticky-v65.js?v=110",
+  "./app-v58.js?v=110",
+  "./app-v47.js?v=110",
+  "./asset-detail-v53.js?v=107",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key)))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+        return response;
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
+
